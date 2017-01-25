1603c6614c3c2a9ee80eb45d6a10161bf68e2482
ac M <mac@macui-macbook-pro-2.local>
2017-01-25 16:32:02 +0900

    Initial Commit

---
+++ githubConnectionXcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubConnectionXcode
+//
+//  Created by Mac on 2017. 1. 25..
+//  Copyright © 2017년 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
